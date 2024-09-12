b2053e0996fdc810d0583db58578b1a2c425cf24
Clean Code <amin.asincorpate@gmail.com>
2024-09-11 18:33:06 -0700

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,11 @@
-
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+const elements = document.qureySelectorAll('.hiden');
+hiddenElement.forEach((el) => observer.observe(el) );
